f70cab067ef8ec4aded21719f23275336a2975f6
Manuel Hintermayr <manuel@hintermayr.net>
2023-09-10 15:56:53 +0200

    (c) test now opens browser and closes it again

--- testautomation/selenium/selenium/CreateSongTest.cs
+++ testautomation/selenium/selenium/CreateSongTest.cs
@@ -1,12 +1,30 @@
+using selenium.Infrastructure;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace selenium
 {
     [TestClass]
     public class CreateSongTest
     {
-        [TestMethod]
-        public void TestMethod()
+        public BaseApplication app { get; private set; }
+
+        [TestInitialize]
+        public void SetUpApplicationBase()
         {
+            app = new BaseApplication();
+        }
+
+        [TestMethod]
+        public void CreateNewSong()
+        {
+            // Arrange
             Assert.IsTrue(true);
+        }
+
+        [TestCleanup]
+        public void ApplicationCleanUp()
+        {
+            app.Dispose();
         }
     }
 }
---
+++ testautomation/selenium/selenium/Infrastructure/BaseApplication.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium.Infrastructure
+{
+    public class BaseApplication : IDisposable
+    {
+        private readonly ChromeDriver _driver;
+
+        public BaseApplication() 
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            _driver = new ChromeDriver(options);
+            _driver.Navigate().GoToUrl("http://localhost:8080/");
+        }
+        public void Dispose()
+        {
+            _driver.Dispose();
+        }
+    }
+}
