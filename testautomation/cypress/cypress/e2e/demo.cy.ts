a7aac1e6af39c1b01171a1345321975da1bd9bd3
Melvin <melvinjohner@hotmail.com>
2023-09-12 09:07:08 +0200

    finished example of playwright

--- testautomation/cypress/cypress/e2e/demo.cy.ts
+++ testautomation/cypress/cypress/e2e/demo.cy.ts
@@ -1,47 +1,62 @@
 describe("demo", () => {
-  after(() => {
+  afterEach(() => {
     cy.request("http://localhost:8081/reset");
   });
 
   it("should create song", () => {
+    const songMetadata = {
+      title: "Schatteboxe",
+      artist: "Zürich West",
+      genre: "Mundart",
+      album: "Love",
+      albumImageUrl:
+        "https://zueriwest.ch/wp-content/uploads/2017/02/Disko_Love.png",
+      youtubeId: "TNW1sCFdDhI",
+      tab: "test",
+      lyrics:
+        "D Sunne schynt dür d Storen uf mys Pult\nU malt es chlyses Vieregg druf us Gold\nEh chasch es mitnäh wes der gfallt\nDe chasch es ha\nDu muesch mer nüt erkläre wed wosch ga\nEh d Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nWed nomau muesch überlege, überleisch\nAber mir muesch nüt erkläre we de geisch\nI probieres z akzeptiere so wies isch\nU grad alles wirdi sicher nid vermisse\nI chume geng no nid ganz drus\nWas du für eini bisch aber chasch sicher si\nDass i das gärn wett wüsse\nD Sunne schynt dür d Storen yy uf ds Pult\nU malt es chlyses Vieregg druf us Gold\nEh d Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nI schatteboxe gäge d Wänd\n\nI probieres z akzeptiere wes so isch\nU grad alles wirdi sicher nid vermisse\nI chume geng no nid ganz drus\nWas du für eini bisch aber chasch sicher si\nDass i das gärn wett wüsse\nD Sunne schynt dür d Storen uf mys Pult\nU malt es chlyses Vieregg druf us Gold\nD Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nI schatteboxe gäge d Wänd\nSchatteboxe gäge d Wänd",
+    };
+
     cy.visit("http://localhost:8080/#/songs/create");
 
-    cy.fixture("testSong").then((songMetadata) => {
-      cy.get('[data-test-id="newSongTitle"]').type(songMetadata.title);
-      cy.get('[data-test-id="newSongArtist"]').type(songMetadata.artist);
-      cy.get('[data-test-id="newSongGenre"]').type(songMetadata.genre);
-      cy.get('[data-test-id="newSongAlbum"]').type(songMetadata.album);
-      cy.get('[data-test-id="newSongAlbumImageUrl"]').type(
-        songMetadata.albumImageUrl
-      );
-      cy.get('[data-test-id="newSongYouTubeId"]').type(songMetadata.youtubeId);
-      cy.get('[data-test-id="newSongStructure"]').type(songMetadata.tab);
-      cy.get('[data-test-id="newSongLyrics"]').type(songMetadata.lyrics, {
-        delay: 0,
-      });
+    cy.get('[data-test-id="newSongTitle"]').type(songMetadata.title);
+    cy.get('[data-test-id="newSongArtist"]').type(songMetadata.artist);
+    cy.get('[data-test-id="newSongGenre"]').type(songMetadata.genre);
+    cy.get('[data-test-id="newSongAlbum"]').type(songMetadata.album);
+    cy.get('[data-test-id="newSongAlbumImageUrl"]').type(
+      songMetadata.albumImageUrl
+    );
+    cy.get('[data-test-id="newSongYouTubeId"]').type(songMetadata.youtubeId);
+    cy.get('[data-test-id="newSongStructure"]').type(songMetadata.tab);
+    cy.get('[data-test-id="newSongLyrics"]').type(songMetadata.lyrics, {
+      delay: 0,
     });
 
-    cy.get('[data-test-id="createSong"]').click();
-    cy.wait(1000);
+    cy.intercept("**/song", (request) => {
+      request.continue((response) => {
+        expect(response.body.title).to.be(songMetadata.title);
+        expect(response.body.artist).to.be(songMetadata.artist);
+        expect(response.body.genre).to.be(songMetadata.genre);
+        expect(response.body.album).to.be(songMetadata.album);
+        expect(response.body.albumImageUrl).to.be(songMetadata.albumImageUrl);
+        expect(response.body.youtubeId).to.be(songMetadata.youtubeId);
+        expect(response.body.tab).to.be(songMetadata.tab);
+        expect(response.body.lyrics).to.be(songMetadata.lyrics);
+      });
+      cy.get('[data-test-id="createSong"]').click();
+    });
   });
 
   it("should find song", () => {
     cy.visit("http://localhost:8080/");
-    cy.fixture("testSong").then((songMetadata) => {
-      cy.get('[data-test-id="search-bar"]').type(songMetadata.title);
-      cy.wait(100);
-      cy.get('[data-test-id="songTitle"]').should(
-        "contain.text",
-        songMetadata.title
-      );
-      cy.get('[data-test-id="songArtist"]').should(
-        "contain.text",
-        songMetadata.artist
-      );
-      cy.get('[data-test-id="songGenre"]').should(
-        "contain.text",
-        songMetadata.genre
-      );
-    });
+    cy.get('[data-test-id="search-bar"]').type("Nevermind");
+    cy.get('[data-test-id="songTitle"]').should("have.length", 1);
+
+    cy.get('[data-test-id="songTitle"]').should("contain.text", "Nevermind");
+    cy.get('[data-test-id="songArtist"]').should("contain.text", "Nirvana");
+    cy.get('[data-test-id="songGenre"]').should(
+      "contain.text",
+      "Alternative Rock"
+    );
   });
 });
--- testautomation/playwright/tests/demo.spec.ts
+++ testautomation/playwright/tests/demo.spec.ts
@@ -1,35 +1,60 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 const songMetadata = {
-  "title": "Schatteboxe",
-  "artist": "Zürich West",
-  "genre": "Mundart",
-  "album": "Love",
-  "albumImageUrl": "https://zueriwest.ch/wp-content/uploads/2017/02/Disko_Love.png",
-  "youtubeId": "TNW1sCFdDhI",
-  "tab": "test",
-  "lyrics": "D Sunne schynt dür d Storen uf mys Pult\nU malt es chlyses Vieregg druf us Gold\nEh chasch es mitnäh wes der gfallt\nDe chasch es ha\nDu muesch mer nüt erkläre wed wosch ga\nEh d Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nWed nomau muesch überlege, überleisch\nAber mir muesch nüt erkläre we de geisch\nI probieres z akzeptiere so wies isch\nU grad alles wirdi sicher nid vermisse\nI chume geng no nid ganz drus\nWas du für eini bisch aber chasch sicher si\nDass i das gärn wett wüsse\nD Sunne schynt dür d Storen yy uf ds Pult\nU malt es chlyses Vieregg druf us Gold\nEh d Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nI schatteboxe gäge d Wänd\n\nI probieres z akzeptiere wes so isch\nU grad alles wirdi sicher nid vermisse\nI chume geng no nid ganz drus\nWas du für eini bisch aber chasch sicher si\nDass i das gärn wett wüsse\nD Sunne schynt dür d Storen uf mys Pult\nU malt es chlyses Vieregg druf us Gold\nD Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nI schatteboxe gäge d Wänd\nSchatteboxe gäge d Wänd"
-}
+  title: "Schatteboxe",
+  artist: "Zürich West",
+  genre: "Mundart",
+  album: "Love",
+  albumImageUrl:
+    "https://zueriwest.ch/wp-content/uploads/2017/02/Disko_Love.png",
+  youtubeId: "TNW1sCFdDhI",
+  tab: "test",
+  lyrics:
+    "D Sunne schynt dür d Storen uf mys Pult\nU malt es chlyses Vieregg druf us Gold\nEh chasch es mitnäh wes der gfallt\nDe chasch es ha\nDu muesch mer nüt erkläre wed wosch ga\nEh d Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nWed nomau muesch überlege, überleisch\nAber mir muesch nüt erkläre we de geisch\nI probieres z akzeptiere so wies isch\nU grad alles wirdi sicher nid vermisse\nI chume geng no nid ganz drus\nWas du für eini bisch aber chasch sicher si\nDass i das gärn wett wüsse\nD Sunne schynt dür d Storen yy uf ds Pult\nU malt es chlyses Vieregg druf us Gold\nEh d Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nI schatteboxe gäge d Wänd\n\nI probieres z akzeptiere wes so isch\nU grad alles wirdi sicher nid vermisse\nI chume geng no nid ganz drus\nWas du für eini bisch aber chasch sicher si\nDass i das gärn wett wüsse\nD Sunne schynt dür d Storen uf mys Pult\nU malt es chlyses Vieregg druf us Gold\nD Stadt isch violett u d Schätte läng\nI weiss nume nid was söll i jetz mit däm?\nI schatteboxe gäge d Wänd\nSchatteboxe gäge d Wänd",
+};
 
-test('should create song', async ({ page }) => {
-  await page.goto('http://localhost:8080/#/songs/create');
+test("should create song", async ({ page }) => {
+  await page.goto("http://localhost:8080/#/songs/create");
 
-  await page.getByTestId('newSongTitle').type(songMetadata.title)
-  await page.getByTestId('newSongArtist').type(songMetadata.artist)
-  await page.getByTestId('newSongGenre').type(songMetadata.genre)
-  await page.getByTestId('newSongAlbum').type(songMetadata.album)
-  await page.getByTestId('newSongAlbumImageUrl').type(songMetadata.albumImageUrl)
-  await page.getByTestId('newSongYouTubeId').type(songMetadata.youtubeId)
-  await page.getByTestId('newSongStructure').type(songMetadata.tab)
-  await page.getByTestId('newSongLyrics').type(songMetadata.lyrics)
+  await page.getByTestId("newSongTitle").type(songMetadata.title);
+  await page.getByTestId("newSongArtist").type(songMetadata.artist);
+  await page.getByTestId("newSongGenre").type(songMetadata.genre);
+  await page.getByTestId("newSongAlbum").type(songMetadata.album);
+  await page
+    .getByTestId("newSongAlbumImageUrl")
+    .type(songMetadata.albumImageUrl);
+  await page.getByTestId("newSongYouTubeId").type(songMetadata.youtubeId);
+  await page.getByTestId("newSongStructure").type(songMetadata.tab);
+  await page.getByTestId("newSongLyrics").type(songMetadata.lyrics);
 
-  await page.getByTestId('createSong').click()
+  const responsePromise = page.waitForResponse("**/songs");
+  await page.getByTestId("createSong").click();
+  const response = await responsePromise;
 
+  expect(response.status()).toBe(200);
 
-
-
+  const createdSong = await response.json();
+  expect(createdSong.title).toBe(songMetadata.title);
+  expect(createdSong.artist).toBe(songMetadata.artist);
+  expect(createdSong.genre).toBe(songMetadata.genre);
+  expect(createdSong.album).toBe(songMetadata.album);
+  expect(createdSong.albumImageUrl).toBe(songMetadata.albumImageUrl);
+  expect(createdSong.youtubeId).toBe(songMetadata.youtubeId);
+  expect(createdSong.tab).toBe(songMetadata.tab);
+  expect(createdSong.lyrics).toBe(songMetadata.lyrics);
 });
 
-test('get started link', async ({ page }) => {
-  
+test("should find song", async ({ page }) => {
+  page.goto("http://localhost:8080/");
+  page.getByTestId("search-bar").type("Nevermind");
+
+  await expect(page.getByTestId("songTitle")).toHaveCount(1);
+
+  await expect(page.getByTestId("songTitle")).toContainText("Nevermind");
+  await expect(page.getByTestId("songArtist")).toContainText("Nirvana");
+  await expect(page.getByTestId("songGenre")).toContainText("Alternative Rock");
+});
+
+test.afterEach(async ({ request }) => {
+  await request.get("http://localhost:8081/reset");
 });
