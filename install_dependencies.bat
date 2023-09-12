b5bef1bff35ae2ec15ac9fef2b5bdd3ddc962ac1
Melvin <melvinjohner@hotmail.com>
2023-09-12 14:10:04 +0200

    added dependency script

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,17 +1,30 @@
 :: Client
+echo "Install Client"
 cd client
-call npm install
+call npm i
 cd ..
 
 :: Server
+echo "Install Server"
 cd server
-call npm install
+call npm i
 cd ..
 
 :: Selenium
-cd testautomation
-cd selenium
+echo "Install Selenium"
+cd testautomation/selenium
 call dotnet build
 cd ..
 
+:: Cypress
+echo "Install Cypress"
+cd cypress
+call npm i
+cd ..
+
+:: Playwirght
+echo "Install Playwright"
+cd playwright
+call npm i
+cd ..
 pause
